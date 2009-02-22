375990b13985036e05c35127d63badc5c41a9436
ahkdiep <ahkdiep@f1d5efd2-00ae-11de-802b-177316e4ac06>
2009-02-22 18:41:29 +0000

    r186639@www:  hai | 2009-02-22 11:36:16 -0700  include header in the distribution

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ import os, sys, shutil
 
 setup(
 	name='ctags',
-	version='1.0',
+	version='1.0.1',
 	description='python ctags bindings',
 	author='Aaron H. K. Diep',
 	author_email='ahkdiep@gmail.com',
