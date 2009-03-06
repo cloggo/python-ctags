a2855340e358893f298c5fa048e876eeba75ee79
ahkdiep <ahkdiep@f1d5efd2-00ae-11de-802b-177316e4ac06>
2009-03-06 22:41:03 +0000

    r187219@www:  root | 2009-03-06 15:39:08 -0700   r187216@www:  hai | 2009-03-06 15:37:11 -0700    r875@DomeServer:  (no author) | 2009-03-06 15:33:09 -0700    remove revision number

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ doclines = __doc__.split("\n")
 
 setup(
 	name='python-ctags',
-	version='1.0.' + "$Rev$",
+	version='1.0.6' 
 	description=doclines[0],
 	long_description="\n".join(doclines[2:]),
 	author='Aaron H. K. Diep',
