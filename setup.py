6b335f75e713c113c6d5e80c04d1eebfec571cb1
Aaron Diep <ahkdiep@gmail.com>
2015-08-04 14:06:15 -0600

    fixed classifiers

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 """Exuberant Ctags indexing python bindings
 
-Exuberant Ctags supports indexing of many modern programming languages. Python 
-is a powerful scriptable dynamic language. Using Python to access Ctags index 
-file is a natural fit in extending an application's capability to examine source 
+Exuberant Ctags supports indexing of many modern programming languages. Python
+is a powerful scriptable dynamic language. Using Python to access Ctags index
+file is a natural fit in extending an application's capability to examine source
 code.
 
-This project wrote a wrapper for readtags.c. I have been using the package in 
-a couple of projects and it has been shown that it could easily handle hundreds 
+This project wrote a wrapper for readtags.c. I have been using the package in
+a couple of projects and it has been shown that it could easily handle hundreds
 source files.
 """
 
-classifiers = """\
-Development Status :: 5 - Production/Stable
-Intended Audience :: Developers
-Programming Language :: Python
-Programming Language :: C
-License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
-Operating System :: MacOS :: MacOS X
-Operating System :: Microsoft :: Windows
-Operating System :: POSIX
-Topic :: Software Development :: Libraries :: Python Modules
-"""
+classifiers = [
+'Development Status :: 5 - Production/Stable',
+'Intended Audience :: Developers',
+'Programming Language :: Python',
+'Programming Language :: C',
+'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+'Operating System :: MacOS :: MacOS X',
+'Operating System :: Microsoft :: Windows',
+'Operating System :: POSIX',
+'Topic :: Software Development :: Libraries :: Python Modules',
+]
 
 from distutils.core import setup
 from distutils.command import build, clean
@@ -32,15 +32,15 @@ doclines = __doc__.split("\n")
 setup(
 	name='python-ctags',
 	version='1.0.6' ,
-	description=doclines[0], 
-	long_description="\n".join(doclines[2:]), 
-	author='Aaron H. K. Diep', 
-	author_email='ahkdiep@gmail.com', 
-	license = 'LGPL', 
-	url='https://github.com/hddmet/python-ctags', 
-	classifiers = filter(None, classifiers.split("\n")), 
-	packages = ['ctags'], 
-	ext_package='ctags', 
+	description=doclines[0],
+	long_description="\n".join(doclines[2:]),
+	author='Aaron H. K. Diep',
+	author_email='ahkdiep@gmail.com',
+	license = 'LGPL',
+	url='https://github.com/hddmet/python-ctags',
+	classifiers = classifiers,
+	packages = ['ctags'],
+	ext_package='ctags',
 	ext_modules=[Extension(
             '_readtags', ['src/readtags.c', 'src/_readtags.c'],
 	include_dirs=['src/include']
